57fc88f8490c54a9f9faa17dcc50f9a6ea8d5e04
Lucas da silva costa <lucas@unicorp.com.br>
2023-11-16 08:51:08 -0300

    mostrando o conteúdo da página com as rotas

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,7 @@ import { Home } from "./Home";
 export function AppRouter() {
     return(
         <Routes>
-            <Route path="/" element={ <Home /> } ></Route>
+            <Route path="/*" element={ <Home /> } ></Route>
 
             {/* <Route path="*" element={ <h1>Not Found</h1> } />*/}
         </Routes>
